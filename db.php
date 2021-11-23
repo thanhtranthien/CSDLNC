47bbf7e7fa6c24a49455047b5b645812e39f88c7
thanhtranthien <50793089+thanhtranthien@users.noreply.github.com>
2021-11-23 10:17:44 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
 echo "Hello"; 
-$servername = "database-1.cocgl5wbv5ga.ap-southeast-1.rds.amazonaws.com:3306";
+$servername = "database-1.cocgl5wbv5ga.ap-southeast-1.rds.amazonaws.com";
 $database = "database-1";
 $username = "admin";
 $password = "12345678";
+$port = "3306";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $database, $port);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
