7d49c8b9b6671e0c5ca7f4f84462a01b28f4b519
thanhtranthien <50793089+thanhtranthien@users.noreply.github.com>
2021-11-23 11:44:11 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 echo "Hello"; 
-$servername = "database-1.cocgl5wbv5ga.ap-southeast-1.rds.amazonaws.com:3306";
+$servername = "database-1.cocgl5wbv5ga.ap-southeast-1.rds.amazonaws.com";
 $database = "database-1";
 $username = "admin";
 $password = "12345678";
@@ -8,7 +8,7 @@ $port = "3306";
 // Create connection
 echo "Hello2";
 try {
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    $conn = mysqli_connect($servername, $username, $password, $database, 3306);
 } catch (mysqli_sql_exception $e) {
     die("Unfortunately, the details you entered for connection are incorrect!");
 }
