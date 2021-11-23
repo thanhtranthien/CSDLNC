a661693e13f1af360845207ea6809d2ad164a2f0
thanhtranthien <50793089+thanhtranthien@users.noreply.github.com>
2021-11-23 09:56:35 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "database-1.cocgl5wbv5ga.ap-southeast-1.rds.amazonaws.com";
+$database = "database-1";
+$username = "admin";
+$password = "12345678";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
